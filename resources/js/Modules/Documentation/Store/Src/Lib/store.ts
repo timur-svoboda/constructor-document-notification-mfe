66efed6c16bb6ed90076a8bc34aa0cfef3eaadfb
Timur Svoboda <svobodatimur@gmail.com>
2024-05-03 04:45:03 +0300

    Fix listeners imports

--- resources/js/Modules/Documentation/Store/Src/Lib/store.ts
+++ resources/js/Modules/Documentation/Store/Src/Lib/store.ts
@@ -1,6 +1,56 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { listenerMiddleware } from "./Listeners/listenerMiddleware";
 import documentsSlice from "./Slices/documentsSlice";
+import {
+    TypedStartListening,
+    createListenerMiddleware,
+} from "@reduxjs/toolkit";
+import { documentsRequested } from "./Actions/documentsActions";
+import { fetchDocuments } from "@Documentation/API";
+import { documentsFetched } from "./Slices/documentsSlice";
+import { selectAllDocuments } from "./Selectors/documentSelectors";
+
+export const listenerMiddleware = createListenerMiddleware();
+
+export default listenerMiddleware;
+
+export const startAppListening =
+    listenerMiddleware.startListening as TypedStartListening<
+        RootState,
+        AppDispatch
+    >;
+
+let requestedDocumentIds: string[] = [];
+
+startAppListening({
+    actionCreator: documentsRequested,
+    effect: async (action, listenerApi) => {
+        listenerApi.cancelActiveListeners();
+
+        requestedDocumentIds = [
+            ...new Set([...requestedDocumentIds, ...action.payload]),
+        ];
+
+        await listenerApi.delay(100);
+
+        const fetchedDocumentIds = selectAllDocuments(
+            listenerApi.getState()
+        ).map(({ id }) => id);
+
+        const uniqueRequestedDocumentIds = requestedDocumentIds.filter(
+            (id) => !fetchedDocumentIds.includes(id)
+        );
+
+        if (uniqueRequestedDocumentIds.length > 0) {
+            listenerApi.dispatch(
+                documentsFetched(
+                    await fetchDocuments({ ids: requestedDocumentIds })
+                )
+            );
+        }
+
+        requestedDocumentIds = [];
+    },
+});
 
 export const store = configureStore({
     reducer: {
--- resources/js/Modules/NotificationSystem/Store/Src/Lib/store.ts
+++ resources/js/Modules/NotificationSystem/Store/Src/Lib/store.ts
@@ -1,7 +1,55 @@
 import { configureStore } from "@reduxjs/toolkit";
 import notificationsSlice from "./Slices/notificationsSlice";
 import statisticSlice from "./Slices/statisticsSlice";
-import { listenerMiddleware } from "./Listeners/listenerMiddleware";
+import { fetchStatistics } from "@NotificationSystem/API";
+import {
+    TypedStartListening,
+    createListenerMiddleware,
+} from "@reduxjs/toolkit";
+import { statisticsRequested } from "./Actions/statisticsActions";
+import { selectAllStatistics } from "./Selectors/statisticSelectors";
+import { statisticsFetched } from "./Slices/statisticsSlice";
+
+export const listenerMiddleware = createListenerMiddleware();
+
+export const startAppListening =
+    listenerMiddleware.startListening as TypedStartListening<
+        RootState,
+        AppDispatch
+    >;
+
+let requestedStatisticIds: string[] = [];
+
+startAppListening({
+    actionCreator: statisticsRequested,
+    effect: async (action, listenerApi) => {
+        listenerApi.cancelActiveListeners();
+
+        requestedStatisticIds = [
+            ...new Set([...requestedStatisticIds, ...action.payload]),
+        ];
+
+        await listenerApi.delay(100);
+
+        const fetchedStatisticIds = selectAllStatistics(
+            listenerApi.getState()
+        ).map(({ id }) => id);
+
+        const uniqueRequestedDocumentIds = requestedStatisticIds.filter(
+            (id) => !fetchedStatisticIds.includes(id)
+        );
+
+        if (uniqueRequestedDocumentIds.length > 0) {
+            listenerApi.dispatch(
+                statisticsFetched(
+                    await fetchStatistics({ ids: requestedStatisticIds })
+                )
+            );
+        }
+
+        requestedStatisticIds = [];
+    },
+});
 
 export const store = configureStore({
     reducer: {
