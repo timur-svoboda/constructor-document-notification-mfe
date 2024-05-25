2afe672ac2851533fb54da1bae9b8dd7dc59e625
Timur Svoboda <svobodatimur@gmail.com>
2024-05-25 19:21:45 +0300

    Disable TemplatePage optimization

--- app/Constructor/Pages/TemplatePage.php
+++ app/Constructor/Pages/TemplatePage.php
@@ -9,64 +9,64 @@ use App\Constructor\Actions\FetchNodes;
 
 class TemplatePage {
     public function __invoke(Request $request) {
-        $nodes = FetchNodes::run()->toArray();
+        // $nodes = FetchNodes::run()->toArray();
 
-        $documents = [];
-        try {
-            $documents = app()->handle(
-                Request::create(
-                    '/api/documentation/fetchDocuments',
-                    'GET',
-                    [
-                        'ids' => collect($nodes)->filter(fn ($node) => $node['type'] === 'document')
-                            ->map(fn ($node) => $node['resourceId'])
-                            ->toArray(),
-                    ],
-                )
-            )->getData();
+        // $documents = [];
+        // try {
+        //     $documents = app()->handle(
+        //         Request::create(
+        //             '/api/documentation/fetchDocuments',
+        //             'GET',
+        //             [
+        //                 'ids' => collect($nodes)->filter(fn ($node) => $node['type'] === 'document')
+        //                     ->map(fn ($node) => $node['resourceId'])
+        //                     ->toArray(),
+        //             ],
+        //         )
+        //     )->getData();
 
-        } catch (\Throwable $throwable) {
-            // Log error
-        }
+        // } catch (\Throwable $throwable) {
+        //     // Log error
+        // }
 
-        $statistics = [];
-        try {
-            $statistics = app()->handle(
-                Request::create(
-                    '/api/notificationSystem/fetchStatistics',
-                    'GET',
-                    [
-                        'ids' => collect($documents)
-                            ->map(fn ($document) => $document->id)
-                            ->toArray(),
-                    ],
-                )
-            )->getData();
-        } catch (\Throwable $throwable) {
-            dd($throwable);
-            // Log error
-        }
+        // $statistics = [];
+        // try {
+        //     $statistics = app()->handle(
+        //         Request::create(
+        //             '/api/notificationSystem/fetchStatistics',
+        //             'GET',
+        //             [
+        //                 'ids' => collect($documents)
+        //                     ->map(fn ($document) => $document->id)
+        //                     ->toArray(),
+        //             ],
+        //         )
+        //     )->getData();
+        // } catch (\Throwable $throwable) {
+        //     dd($throwable);
+        //     // Log error
+        // }
 
-        $notifications = [];
-        try {
-            $notifications = app()->handle(
-                Request::create(
-                    '/api/notificationSystem/fetchNotifications',
-                    'GET',
-                    [
-                        'isRead' => false,
-                    ],
-                )
-            )->getData();
-        } catch (\Throwable $throwable) {
-            // Log error
-        }
+        // $notifications = [];
+        // try {
+        //     $notifications = app()->handle(
+        //         Request::create(
+        //             '/api/notificationSystem/fetchNotifications',
+        //             'GET',
+        //             [
+        //                 'isRead' => false,
+        //             ],
+        //         )
+        //     )->getData();
+        // } catch (\Throwable $throwable) {
+        //     // Log error
+        // }
         
         return Inertia::render('Constructor/Pages/Src/Lib/TemplatePage/TemplatePage', [
-            'nodes' => $nodes,
-            'documents' => $documents,
-            'statistics' => $statistics,
-            'notifications' => $notifications,
+            // 'nodes' => $nodes,
+            // 'documents' => $documents,
+            // 'statistics' => $statistics,
+            // 'notifications' => $notifications,
         ]);
     }
 }
